0e1530a3f465bbc7c13152aef3dd9a2917644222
sisaphilip <sisaphilip@gmail.com>
2024-02-10 23:29:23 +0300

    20240210

--- 01_combinational_logic/01_08_not_gate_using_mux.sv
+++ 01_combinational_logic/01_08_not_gate_using_mux.sv
@@ -17,6 +17,14 @@ module not_gate_using_mux
     output o
 );
 
+
+parameter in_1 = 0;
+parameter in_2 = 1;
+
+mux mux_inst (.d0(in_2), .d1(in_1), .sel(i), .y(o));
+    //mux_inst2 (.d0(in_2), .d1(), .sel(in_1), .y(o));
+
+
   // TODO
 
   // Implement not gate using instance(s) of mux,
