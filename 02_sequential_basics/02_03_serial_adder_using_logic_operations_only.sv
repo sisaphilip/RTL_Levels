89f0bd2f87a7b4bdbac0dada552960a38382fec0
sisaphilip <sisaphilip@gmail.com>
2024-02-18 19:48:23 +0300

    feijei

--- 02_sequential_basics/02_03_serial_adder_using_logic_operations_only.sv
+++ 02_sequential_basics/02_03_serial_adder_using_logic_operations_only.sv
@@ -1,7 +1,6 @@
 //----------------------------------------------------------------------------
 // Example
 //----------------------------------------------------------------------------
-
 module serial_adder
 (
   input  clk,
@@ -10,27 +9,21 @@ module serial_adder
   input  b,
   output sum
 );
-
-  // Note:
   // carry_d represents the cominational data input to the carry register.
-
-  logic carry;
-  wire carry_d;
-
-  assign { carry_d, sum } = a + b + carry;
-
-  always_ff @ (posedge clk)
-    if (rst)
-      carry <= '0;
-    else
-      carry <= carry_d;
-
+  //logic carry;
+  //wire carry_d;
+  //assign { carry_d, sum } = a + b + carry;
+  //always_ff @ (posedge clk)
+  
+    //if (rst)
+   //   carry <= '0;
+    //else
+      //carry <= carry_d;
+  
 endmodule
-
 //----------------------------------------------------------------------------
 // Task
 //----------------------------------------------------------------------------
-
 module serial_adder_using_logic_operations_only
 (
   input  clk,
@@ -40,35 +33,31 @@ module serial_adder_using_logic_operations_only
   output sum
 );
 
-  // Task:
-  // Implement a serial adder using only ^ (XOR), | (OR), & (AND), ~ (NOT) bitwise operations.
-  //
-  // Notes:
-  // See Harris & Harris book
-  // or https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder webpage
-  // for information about the 1-bit full adder implementation.
-  //
-  // See the testbench for the output format ($display task).
+ assign sum   = a ^ b;
+// assign carry = a & b;
 
-
+ always@ (posedge clk)
+   if (rst)
+     a <= '0;
+// Task:
+// Implement a serial adder using only ^ (XOR), | (OR), & (AND), ~ (NOT) bitwise operations.
+// Notes:
+// See Harris & Harris book
+// or https://en.wikipedia.org/wiki/Adder_(electronics)#Full_adder webpage
+// for information about the 1-bit full adder implementation.
+// See the testbench for the output format ($display task).
 endmodule
-
 //----------------------------------------------------------------------------
 // Testbench
 //----------------------------------------------------------------------------
-
 module testbench;
-
   logic clk;
-
   initial
   begin
     clk = '0;
-
     forever
       # 500 clk = ~ clk;
   end
-
   logic rst;
 
   initial
@@ -83,7 +72,7 @@ module testbench;
   logic a, b, sa_sum, salo_sum;
   serial_adder                             sa   (.sum (sa_sum),   .*);
   serial_adder_using_logic_operations_only salo (.sum (salo_sum), .*);
-
+  
   localparam n = 16;
 
   // Sequence of input values
@@ -93,7 +82,7 @@ module testbench;
   // Expected sequence of correct output values
   localparam [0 : n - 1] seq_sa_sum   = 16'b0110_0111_0100_0101;
   localparam [0 : n - 1] seq_salo_sum = 16'b0110_0111_0100_0101;
-
+  
   initial
   begin
     @ (negedge rst);
